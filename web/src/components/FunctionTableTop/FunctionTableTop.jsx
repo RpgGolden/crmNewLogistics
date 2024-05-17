b1b40ac2f349ce2edc9d184bebb24ac62c8dc879
Никита <100412266+Niks6455@users.noreply.github.com>
2024-05-17 09:27:13 +0300

    Filterrr

--- web/src/components/FunctionTableTop/FunctionTableTop.jsx
+++ web/src/components/FunctionTableTop/FunctionTableTop.jsx
@@ -3,7 +3,7 @@ import styles from "./FunctionTableTop.module.scss";
 import List from "../../UI/List/List";
 import Input from "../../UI/Input/Input";
 import DataContext from "../../context";
-
+import { testData } from "../../DataApi";
 
 function FunctionTableTop() {
     const TableData= [
@@ -23,21 +23,38 @@ function FunctionTableTop() {
     const defaultValue = "Заказы" 
     const { context } = React.useContext(DataContext);
     const [textSearchTableData, settextSearchTableData] = useState("")
-    
-    useEffect(()=>{
-        console.log(textSearchTableData)
-        context.setsearchDataForTable(textSearchTableData)
-        
-    },[textSearchTableData])
+
+    const filteredData = (searchText) => {
+        const filteredData = testData.filter(item => {
+            for (let key in item) {
+                if (typeof item[key] === 'string' && item[key].toLowerCase().includes(searchText.toLowerCase())) {
+                    return true;
+                }
+            }
+            return false;
+        });
+        context.setTableData(filteredData); // Move this line here
+    }
+
+    useEffect(() => {
+        if (textSearchTableData) {
+            filteredData(textSearchTableData);
+        } else {
+            context.setTableData(testData); // Set original data if search text is empty
+        }
+    }, [textSearchTableData]);
 
     return (
         <>
             <div className={styles.FunctionTableTop}>
                 <div className={styles.container}>
-                    <List data={TableData} defaultValue={defaultValue}/>
+                    <List  defaultValue={defaultValue}/>
                     <div className={styles.searchForTable}>
                         <Input placeholder={"Поиск..."} settextSearchTableData={settextSearchTableData}/>
                         <img src="./img/Search_light.png"/>
+                    </div>
+                    <div class={filterMenu}>
+
                     </div>
                 </div>
             </div>
