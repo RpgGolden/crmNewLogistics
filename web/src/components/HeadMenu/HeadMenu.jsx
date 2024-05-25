68bd3077639aba909389f624f1b020c4f9039aa7
Artur <artur.tonoyan.04@list.ru>
2024-05-25 10:59:32 +0300

    file v 323

--- web/src/components/HeadMenu/HeadMenu.jsx
+++ web/src/components/HeadMenu/HeadMenu.jsx
@@ -77,12 +77,12 @@ function HeadMenu({ state, setFiltredData, filtredData }) {
     console.log(context.selectedTr);
     apiGetFile(context.selectedTr).then((response) => {
       console.log(response);
-      const url = window.URL.createObjectURL(new Blob([response.data]));
-      const link = document.createElement("a");
-      link.href = url;
-      link.setAttribute("download", "расчетный_лист.docx");
-      document.body.appendChild(link);
-      link.click();
+      // const url = window.URL.createObjectURL(new Blob([response.data]));
+      // const link = document.createElement("a");
+      // link.href = url;
+      // link.setAttribute("download", "расчетный_лист.docx");
+      // document.body.appendChild(link);
+      // link.click();
     });
   };
 
